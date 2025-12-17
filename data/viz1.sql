51a078a8206fafcb5c2345303caf91e1269c51aa
Henry <chehenry200@gmail.com>
2025-12-17 22:05:17 +0100

    added select clause for viz1 and viz2

--- data/viz1.sql
+++ data/viz1.sql
@@ -1,1 +1,19 @@
 USE AdventureWorks2025
+
+Select * from Production.ProductCategory
+
+Select * from Production.Product
+
+Select * from Production.ProductSubcategory
+
+ 
+Select 
+PC.Name as CategoryName, 
+PS.Name as SubcategoryName, 
+P.Name as ProductName
+From Production.ProductCategory as PC
+Join Production.ProductSubcategory as PS
+    On PC.ProductCategoryID = PS.ProductCategoryID  
+Join Production.Product as P
+    On PS.ProductSubcategoryID = P.ProductSubcategoryID
+Order by CategoryName, SubcategoryName, ProductName  
--- data/viz2.sql
+++ data/viz2.sql
@@ -1,1 +1,10 @@
 USE AdventureWorks2025
+
+select * from Production.ProductCategory
+
+
+select * from Production.ProductSubcategory
+
+select * from Production.Product
+
+select * from sales.SalesOrderDetail
