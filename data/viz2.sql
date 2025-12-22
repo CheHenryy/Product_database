bf716dd2547d0c895e410a6426c69503c2c0509b
Henry <chehenry200@gmail.com>
2025-12-22 01:18:27 +0100

    added one more visualisation- viz 2

--- data/viz2.sql
+++ data/viz2.sql
@@ -11,15 +11,12 @@ select * from sales.SalesOrderDetail
 
 SELECT 
     PC.Name AS CategoryName, 
-    SOD.OrderQty AS QuantitySold,
-    SOD.UnitPrice AS UnitPrice,
-    SOD.OrderQty * SOD.UnitPrice AS TotalSales,
-    COUNT(DISTINCT P.ProductID) as ProductCount,
-    SOD.LineTotal AS LineTotal
+    SUM(SOD.LineTotal) AS LineTotal,
+    SUM(SOD.OrderQty * SOD.UnitPrice) AS TotalSales --verifying this figure with thr LineTotal
 FROM Production.ProductCategory AS PC 
 INNER JOIN Production.ProductSubcategory AS PS ON PC.ProductCategoryID = PS.ProductCategoryID  
 INNER JOIN Production.Product AS P ON PS.ProductSubcategoryID = P.ProductSubcategoryID
 INNER JOIN sales.SalesOrderDetail AS SOD ON P.ProductID = SOD.ProductID
-GROUP BY PC.Name
-ORDER BY ProductCount DESC;
- 
+GROUP BY PC.Name     
+ORDER BY SUM(SOD.LineTotal) DESC, SUM(SOD.OrderQty * SOD.UnitPrice) DESC;
+
--- data/viz5.sql
+++ data/viz5.sql
@@ -1,7 +1,16 @@
 USE AdventureWorks2025
 
-
-
-SELECT * FROM Production.Product 
-
 SELECT * FROM Sales.SalesOrderDetail
+
+SELECT * FROM Production.Product
+
+
+SELECT 
+P.Name as ProductName,
+COUNT(DISTINCT P.Name) AS ProductCount,
+SUM(SOD.OrderQty * SOD.UnitPrice) as TotalSales
+FROM Sales.SalesOrderDetail SOD
+JOIN Production.Product P
+ON SOD.ProductID = P.ProductID
+GROUP BY P.Name
+ORDER BY TotalSales DESC; 
