ac81d2a60003953dcd4c4a08118c327b826f6c04
Henry <chehenry200@gmail.com>
2025-12-24 02:27:50 +0100

    Added viz 6

--- data/viz6.sql
+++ data/viz6.sql
@@ -7,3 +7,17 @@ SELECT * FROM Sales.SalesTerritory
 SELECT * FROM Sales.SalesOrderHeader 
 
 SELECT * FROM Sales.Customer
+
+
+SELECT
+    TERR.Name AS TerritoryName,   
+    COUNT(DISTINCT CUST.CustomerID) AS CustomerCount,
+    SUM(SOH.TotalDue) AS TotalSales,
+    TERR.[Group] AS Continent
+FROM Sales.SalesOrderHeader AS SOH
+JOIN Sales.Customer AS CUST
+ON SOH.CustomerID = CUST.CustomerID
+JOIN Sales.SalesTerritory AS TERR
+ON CUST.TerritoryID = TERR.TerritoryID
+GROUP BY TERR.Name, TERR.[Group]
+ORDER BY TotalSales DESC, CustomerCount DESC;
